c12aaf815f59f2d7441315e476cbeeeab68758eb
theklayd <clydesantos1028@gmail.com>
2018-10-25 10:10:45 +0800

    ssadasd

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var express = require('express'),
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/views'))
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
