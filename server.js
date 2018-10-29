8b7033978fb8ace2ef5a46b4f79f30131af0e7b3
theklayd <clydesantos1028@gmail.com>
2018-10-29 21:20:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
-app.get('/', function (req, res) {
-  // res.render('index.html');
+app.get('**', function (req, res) {
+  res.render('index.html');
 });
 
 // error handling
